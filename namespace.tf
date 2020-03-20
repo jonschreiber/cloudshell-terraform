c24178d28d317c5a08fffeaf7976400a8d1d7e88
Jonathan Schreiber <jonschreiber@gmail.com>
2020-03-20 21:20:12 +0000

    initial commit of object store bucket stack

---
+++ namespace.tf
@@ -0,0 +1,18 @@
+data "oci_objectstorage_namespace" "ns" {
+  #Optional
+  compartment_id = "${var.compartment_ocid}"
+}
+
+output namespace {
+  value = "${data.oci_objectstorage_namespace.ns.namespace}"
+}
+
+resource "oci_objectstorage_namespace_metadata" "namespace-metadata1" {
+  namespace                    = "${data.oci_objectstorage_namespace.ns.namespace}"
+  default_s3compartment_id     = "${var.compartment_ocid}"
+  default_swift_compartment_id = "${var.compartment_ocid}"
+}
+
+data oci_objectstorage_namespace_metadata namespace-metadata1 {
+  namespace = "${data.oci_objectstorage_namespace.ns.namespace}"
+}
